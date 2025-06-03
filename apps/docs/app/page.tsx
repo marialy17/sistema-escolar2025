b01889d65f23cae87b61674e68988905ad5442a2
Manuel Mdz <manuel.mendoza@g-tech.io>
2025-06-03 11:24:51 -0600

    Hola mundo Alex

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -38,6 +38,8 @@ export default function Home() {
           <li>Save and see your changes instantly.</li>
         </ol>
 
+        <h1>Hola mundo Alex</h1>
+
         <div className={styles.ctas}>
           <a
             className={styles.primary}
